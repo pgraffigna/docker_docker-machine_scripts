15b6479192199ac2b88c89055a5f30d0c5ef3df4
pgraffigna <69170202+pgraffigna@users.noreply.github.com>
2020-08-20 12:21:02 -0300

    Update docker-machine.sh

--- docker-machine.sh
+++ docker-machine.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-echo "++ Instalación de docker-machine ++"
 repo=https://github.com/docker/machine/releases/download/v0.16.0
 
+echo "++ Instalación de docker-machine ++"
 curl -Ls $repo/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine
 sudo mv /tmp/docker-machine /usr/local/bin/docker-machine
 chmod +x /usr/local/bin/docker-machine
